31ca22cb6122e763dcac7cf5fb221535bed36093
Kevin Aguilera <kevin.lovo@gmail.com>
2021-09-10 19:08:10 -0500

    Cambios menores

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -16,9 +16,6 @@ const Navigation = () => {
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
-        cardStyle: {
-          backgroundColor: 'white'
-        }
       }}
     >
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
